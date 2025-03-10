74aa384d5322c0820ef91afe1618887ed19975ef
Keronos-RBX <naix7123@gmail.com>
2025-03-10 17:24:06 -0600

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,3 +1,5 @@
+--All creds for this ui lib go to Hydra UI Lib.
+
 local UILibrary = {}
 --// Modules
 
@@ -3265,7 +3267,7 @@ function UILibrary.new(gameName, userId, rank)
     local Drag = Draggable.Drag(window.MainUI, Frame)
 
     --// Customize the GUI
-    window.Watermark.Text = ("RobuxFarm-Kero | %s | %s"):format(userId, gameName)
+    window.Watermark.Text = ("RobuxFarm.Kero | %s | %s"):format(userId, gameName)
     local userinfo = window.MainUI.Sidebar.ContentHolder.UserInfo.Content
     userinfo.Rank.Text = rank
     userinfo.Title.Text = userId
