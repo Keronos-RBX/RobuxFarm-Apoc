e578fcf812e0186feafe324818bffd43fee5cb7a
Keronos-RBX <naix7123@gmail.com>
2025-03-11 02:44:55 -0600

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,6 +1,6 @@
 --(Most) creds for this ui lib go to Hydra Ui Lib, but i added some stuff myself (keronos/naix)
 print("Loading v1.01 of ui lib - Keronos")
-print("This is patch 0.001 of the Ui Lib")
+print("This is patch 0.002 of the Ui Lib")
 
 local UILibrary = {}
 --// Modules
@@ -287,8 +287,7 @@ local function getObjGen()
                 UIPadding_14 = Instance.new("UIPadding"),
                 Notification_3 = Instance.new("Frame"),
                 UICorner_23 = Instance.new("UICorner"),
-                UIAspectRatioConstraint_17 = Instance.new("UIAspectRatioConstraint"),
-                Gui = {}
+                UIAspectRatioConstraint_17 = Instance.new("UIAspectRatioConstraint")
             }
 
             --Properties:
