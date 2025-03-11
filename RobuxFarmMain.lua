9d49ad93a3763afa064f7d6cf91fd56faee7c085
Keronos-RBX <naix7123@gmail.com>
2025-03-10 21:40:40 -0600

    Update RobuxFarmMain.lua

--- RobuxFarmMain.lua
+++ RobuxFarmMain.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Keronos-RBX/RobuxFarm-Apoc/refs/heads/main/Functions.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Keronos-RBX/RobuxFarm-Apoc/refs/heads/main/RunUI.lua"))()
+
