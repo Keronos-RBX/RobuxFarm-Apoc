8498491b973edcb596b6e468d4f185e68f0cff4e
Keronos-RBX <naix7123@gmail.com>
2025-03-11 18:05:53 -0600

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,6 +1,6 @@
 --(Most) creds for this ui lib go to Hydra Ui Lib, but i added some stuff myself (keronos/naix)
 print("Loading v1.01 of ui lib - Keronos")
-print("This is patch 0.012 of the Ui Lib")
+print("This is patch 0.014 of the Ui Lib")
 
 local UILibrary = {}
 --// Modules
