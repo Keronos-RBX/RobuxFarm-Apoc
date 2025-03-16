a5301fa478cc58783cf9bb31bdcf6e931725cdf5
Keronos-RBX <naix7123@gmail.com>
2025-03-15 19:35:47 -0600

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -7,7 +7,7 @@ if getgenv().UIIdentifier ~= CurrentID then
     error("Mismatching instance id's, stopping function")
 end
 
-print("Loading functions - Keronos RobuxFarm.Kero V1.00 patch 0.010")
+print("Loading functions - Keronos RobuxFarm.Kero V1.00 patch 0.011")
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
@@ -405,8 +405,6 @@ function M.StopAll()
     --error("[Functions] All features forcibly stopped, keybinds disconnected, script disabled.")
 end
 
-
---------------------------------------------------------------------------------
 for k,v in pairs(M) do
     getgenv().ApocFunctions[k] = v
 end
